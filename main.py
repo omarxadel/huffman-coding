0abf64985d592d069e0204d6505ef0b2d6130b7b
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-01-04 15:40:53 +0200

    Initial Commit
    [x] Add main.py
    [x] Add driver's code to main.py
    [x] Create a.txt as a test file
    [x] Create .gitignore

---
+++ main.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+
+    # GET FILE NAME
+    filename = input("Enter file name ")
+    while True:
+        try:
+            f = open(filename, 'r', encoding="utf8")
+            break
+        except IOError:
+            filename = input("Enter a valid file name ")
+
+    # GET OPERATION TYPE ( COMPRESSION or DECOMPRESSION )
+    while True:
+        op_type = int(input("Enter the mode (0 for Compression | 1 for Decompression) "))
+        if op_type == 0:
+            print("Compression")
+            break
+        elif op_type == 1:
+            print("Decompression")
+            break
+        else:
+            print("Please enter a valid number")
+            continue
