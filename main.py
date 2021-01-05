6c5b63edb6f965844a13da41dafac1b309bda0ec
Omar Adel <48330931+omarxadel@users.noreply.github.com>
2021-01-05 18:58:06 +0200

    Update main.py
    Add function definitions

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-if __name__ == '__main__':
+def frequency_map(data):
+    return None
 
+
+# def huffman_coding(map):#TODO:ADD HUFFMAN CODING
+
+def decompress():
+    print("Decompression")
+
+
+def compress():
+    print("Compress")
+
+
+if __name__ == '__main__':
     # GET FILE NAME
     filename = input("Enter file name ")
     while True:
@@ -13,11 +26,12 @@ if __name__ == '__main__':
     while True:
         op_type = int(input("Enter the mode (0 for Compression | 1 for Decompression) "))
         if op_type == 0:
-            print("Compression")
+            compress()
             break
         elif op_type == 1:
-            print("Decompression")
+            decompress()
             break
         else:
             print("Please enter a valid number")
             continue
+
